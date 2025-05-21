51683fcc36c62efb3c3e0b80cd8e74113790d707
Grey <grey@Mac-mini.local>
2025-05-22 01:03:55 +0800

    org 0x7c00

--- boot_sect.asm
+++ boot_sect.asm
@@ -1,3 +1,4 @@
+[org 0x7c00]
 mov ah, 0x0e
 
 mov al, the_secret
@@ -7,11 +8,11 @@ mov al, [the_secret]
 int 0x10
 
 mov bx, the_secret
-add bx, 0x7c00
+;add bx, 0x7c00
 mov al, [bx]
 int 0x10
 
-mov al, [0x7c1d]
+mov al, [0x7c19]
 int 0x10
 
 
