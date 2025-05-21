28abcd129f596b8da2aed8fbce1671d305949fb2
Grey <grey@Mac-mini.local>
2025-05-22 00:44:41 +0800

    absolute address

--- boot_sect.asm
+++ boot_sect.asm
@@ -1,15 +1,24 @@
-mov ah, 0x0e ; tty mode
-mov al, 'H'
-int 0x10
-mov al, 'e'
-int 0x10
-mov al, 'l'
-int 0x10
-int 0x10 ; 'l' is still on al, remember?
-mov al, 'o'
+mov ah, 0x0e
+
+mov al, the_secret
 int 0x10
 
+mov al, [the_secret]
+int 0x10
+
+mov bx, the_secret
+add bx, 0x7c00
+mov al, [bx]
+int 0x10
+
+mov al, [0x7c1d]
+int 0x10
+
+
 jmp $ ; jump to current address = infinite loop
+
+the_secret:
+    db "X"
 
 ; padding and magic number
 times 510 - ($-$$) db 0
